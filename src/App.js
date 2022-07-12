89c7251377a38ce80b3cd4d1bed8a0bb49ec1547
Lucas Vuletic <92898328+Karadarevic@users.noreply.github.com>
2022-07-12 18:59:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React from "react";
 import "./App.css";
-import imageHeader from "./images/image-header-desktop.jpg";
-import imageMobile from "./images/image-header-mobile.jpg";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   return (
     <div className="fullbox">
-      <div className="card">
-        <div className="center">
-          <div className="main">
-            <p className="line">Get insights that help your business grow.</p>
-          </div>
-          <div className="description">
-            <p className="text">
-              {" "}
-              Discover the benefits of data analytics and make better decisions
-              regarding revenue, costumer experience, and overall eficiency
-            </p>
-          </div>
-          <div className="status">
-            <ul>
-              <li>10k+</li>
-              <label>COMPANIES</label>
-            </ul>
-            <ul>
-              <li>314</li>
-              <label>TEMPLATES</label>
-            </ul>
-            <ul>
-              {" "}
-              <li>12M+</li>
-              <label>QUERIES</label>
-            </ul>
-          </div>
+      <div className="center">
+        <h1>
+          Get insights that help <br />
+          your business grow.
+        </h1>
+        <p>
+          {" "}
+          Discover the benefits of data analytics and make <br />
+          better decisions regarding revenue, costumer
+          <br /> experience, and overall eficiency
+        </p>
+        <div className="status">
+          <ul>
+            <li>10k+</li>
+            <label>COMPANIES</label>
+          </ul>
+          <ul>
+            <li>314</li>
+            <label>TEMPLATES</label>
+          </ul>
+          <ul>
+            {" "}
+            <li>12M+</li>
+            <label>QUERIES</label>
+          </ul>
         </div>
-        <div className="hero"></div>
       </div>
+      <div className="hero"></div>
     </div>
   );
 };
